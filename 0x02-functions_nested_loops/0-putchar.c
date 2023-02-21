4164e4757b42dcf601053034ad6eda3ad0d64cea
Andile <azipholuthuli@gmail.com>
2023-02-21 06:51:45 -0800

    put2

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,9 @@
-#include "main.h"
+#include <main.h>
+#include <stdio.h>
+
 int main()
 {
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int cnt, s_z;
-
-	s_z = sizeof(str) / sizeof(int);
-	for (cnt = 0; cnt < s_z; cnt++)
-	{
-		_putchar(str[cnt]);
-	}
-	_putchar('\n');
+	write(1, "_putchar", strlen("_putchar"));
 	return (0);
 }
 
